a531b7d13bed9e03efc4b2f9c07fe480d78f6128
Frenco Jobs <frenco.willian@gmail.com>
2018-11-04 10:08:14 +0630

    style: Added eslint

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 import {
-    isProxySupported,
-    $$Variables,
-    createMap,
-    createArrayMap,
-    createObservableMap,
-    depManager,
-    isObject,
-    err,
-    recommend
+  isProxySupported,
+  $$Variables,
+  createMap,
+  createArrayMap,
+  createObservableMap,
+  depManager,
+  isObject,
+  err,
+  recommend
 } from './utils'
 
 let Interpolaters = {}
-export let Variables = $$Variables 
+export let Variables = $$Variables
 /**
  * Transform an object into a Reactive Object, by Proxying or getter/setter configuring
  * @export
@@ -19,116 +19,113 @@ export let Variables = $$Variables
  * @param {Function} associator allow the user to pass the functions, asscoiated the reactive object
  * @returns Object
  */
-export function act(reactable, associator){
-    let map = createMap({})
-    let commonMap = createArrayMap({})
-    let states = JSON.parse(JSON.stringify(reactable))
-    associator(depManager(map,reactable,commonMap,states))
-    let observableMap = createObservableMap(map)
-    if(isProxySupported && !Variables.__polyfillTest__){
-        return new Proxy(reactable,{
-            get(target,prop){
-                if(commonMap.getter.length > 0) commonMap.getter.forEach(work => work.bind(target)(target,prop))
-                if(observableMap.getter.hasOwnProperty(prop)){
-                    observableMap.getter[prop]
-                    .subscribe({ next: w => w.bind(target)(target,prop) })
-                }
-                return getValueOf(target[prop],target);
-            },
-            set(target,prop,newValue){
-                target[prop] = newValue
-                if(commonMap.setter.length > 0) commonMap.setter.forEach(work => work.bind(target)(target,prop))
-                if(observableMap.setter.hasOwnProperty(prop)){
-                    observableMap.setter[prop]
-                    .subscribe({ next: w =>w.bind(target)(target,prop) })
-                }
-                states[prop] = newValue
-                return true
-            },
-            deleteProperty(target,prop){
-                if(prop in target){
-                    delete target[prop]
-                    if(commonMap.deleter.length > 0) commonMap.deleter.forEach(work => work.bind(target)(target,prop))
-                    if(observableMap.deleter.hasOwnProperty(prop)){
-                        observableMap.deleter[prop]
-                        .subscribe({ next: w => w.bind(target)(target,prop) })
-                    }
-                    return true;
-                } else /* istanbul ignore next */{
-                    err(`Property Not Found : ${prop}`)
-                    return false;
-                }
-            }
-        })
-    } else {
-    function defineProp(target){
-        Object.keys(target).forEach(prop =>{
-            let interValue = target[prop]
-            Object.defineProperty(target,prop,{
-                get(){
-                    if(commonMap.getter.length > 0) commonMap.getter.forEach(work => work.bind(target)(target,prop))
-                    if(observableMap.getter.hasOwnProperty(prop)){
-                        observableMap.getter[prop]
-                        .subscribe({ next: w => w.bind(target)(target,prop) })
-                    }
-                    return getValueOf(interValue,target)
-                },
-                set(newValue){
-                    interValue = newValue
-                    if(commonMap.setter.length > 0) commonMap.setter.forEach(work => work.bind(target)(target,prop))
-                    if(observableMap.setter.hasOwnProperty(prop)){
-                        observableMap.setter[prop]
-                        .subscribe({ next: w =>w.bind(target)(target,prop) })
-                    }
-                    states[prop] = newValue
-                    return true
-                }
-            })
-        })
-    }
+export function act (reactable, associator) {
+  let map = createMap({})
+  let commonMap = createArrayMap({})
+  let states = JSON.parse(JSON.stringify(reactable))
+  associator(depManager(map, reactable, commonMap, states))
+  let observableMap = createObservableMap(map)
+  if (isProxySupported && !Variables.__polyfillTest__) {
+    return new Proxy(reactable, {
+      get (target, prop) {
+        if (commonMap.getter.length > 0) commonMap.getter.forEach(work => work.bind(target)(target, prop))
+        if (observableMap.getter.hasOwnProperty(prop)) {
+          observableMap.getter[prop]
+            .subscribe({ next: w => w.bind(target)(target, prop) })
+        }
+        return getValueOf(target[prop], target)
+      },
+      set (target, prop, newValue) {
+        target[prop] = newValue
+        if (commonMap.setter.length > 0) commonMap.setter.forEach(work => work.bind(target)(target, prop))
+        if (observableMap.setter.hasOwnProperty(prop)) {
+          observableMap.setter[prop]
+            .subscribe({ next: w => w.bind(target)(target, prop) })
+        }
+        states[prop] = newValue
+        return true
+      },
+      deleteProperty (target, prop) {
+        if (prop in target) {
+          delete target[prop]
+          if (commonMap.deleter.length > 0) commonMap.deleter.forEach(work => work.bind(target)(target, prop))
+          if (observableMap.deleter.hasOwnProperty(prop)) {
+            observableMap.deleter[prop]
+              .subscribe({ next: w => w.bind(target)(target, prop) })
+          }
+          return true
+        } else /* istanbul ignore next */{
+          err(`Property Not Found : ${prop}`)
+          return false
+        }
+      }
+    })
+  } else {
+    Object.keys(reactable).forEach(prop => {
+      let interValue = reactable[prop]
+      Object.defineProperty(reactable, prop, {
+        get () {
+          if (commonMap.getter.length > 0) commonMap.getter.forEach(work => work.bind(reactable)(reactable, prop))
+          if (observableMap.getter.hasOwnProperty(prop)) {
+            observableMap.getter[prop]
+              .subscribe({ next: w => w.bind(reactable)(reactable, prop) })
+          }
+          return getValueOf(interValue, reactable)
+        },
+        set (newValue) {
+          interValue = newValue
+          if (commonMap.setter.length > 0) commonMap.setter.forEach(work => work.bind(reactable)(reactable, prop))
+          if (observableMap.setter.hasOwnProperty(prop)) {
+            observableMap.setter[prop]
+              .subscribe({ next: w => w.bind(reactable)(reactable, prop) })
+          }
+          states[prop] = newValue
+          return true
+        }
+      })
+    })
     recommend(`Unfortunately, your browser doesn't support Proxy,so as a polyfill, we used ES5 getters and setters,some functions will not be available in this browser.`)
-    defineProp(reactable);
     return reactable
-}
+  }
 }
 /**
  * Define an interpolater, for dynamically changing values by transforming it
  * @export
  * @class Interpolater
  */
-export class Interpolater{
-    constructor(type,consumer){
-            if(!Interpolaters.hasOwnProperty(type)){
-        Interpolaters[type] = consumer
+export class Interpolater {
+  constructor (type, consumer) {
+    if (!Interpolaters.hasOwnProperty(type)) {
+      Interpolaters[type] = consumer
     } else /* istanbul ignore next */{
-        err('Interpolater already exist.')
+      err('Interpolater already exist.')
     }
-    }
+  }
 }
 /**
  * Check and work if a property is in interpolater
- * @param {property} val 
- * @param {Object} self 
+ * @param {property} val
+ * @param {Object} self
  */
-function getValueOf(val,self){
-    let result = val
-    if(isObject(val) && val.hasOwnProperty('$$type')){
-        result = Interpolaters.hasOwnProperty(val.$$type)
-        ? Interpolaters[val.$$type](val,self)
-        : undefined
-    }
-    return result;
+function getValueOf (val, self) {
+  let result = val
+  if (isObject(val) && val.hasOwnProperty('$$type')) {
+    result = Interpolaters.hasOwnProperty(val.$$type)
+      ? Interpolaters[val.$$type](val, self)
+      : undefined
+  }
+  return result
 }
-new Interpolater('Releaser',function(config,self){
-    return config.$$value(self)
+new Interpolater('Releaser', function (config, self) {
+  return config.$$value(self)
 })
 /**
  * Release values dynamically by Releaser
  * @param {Function} fn function that will run everytime the property is called
  */
-export function Releaser(fn){
-    return {
-        $$type: 'Releaser',
-        $$value: fn
-    }
+export function Releaser (fn) {
+  return {
+    $$type: 'Releaser',
+    $$value: fn
+  }
 }
--- src/utils.js
+++ src/utils.js
@@ -1,16 +1,16 @@
-export let isProxySupported = (typeof Proxy !== undefined)
-export let inBrowser = (typeof window !== undefined)
-export let hasConsole = (typeof console !== undefined)
+export let isProxySupported = (typeof Proxy !== 'undefined')
+export let inBrowser = (typeof window !== 'undefined')
+export let hasConsole = (typeof console !== 'undefined')
 export let $$Variables = {
-    __polyfillTest__: false,
-    __version__: '0.0.0-development'
+  __polyfillTest__: false,
+  __version__: '0.0.0-development'
 }
 
 /**
  * Check if it's an Object or not, excluded `null`
  */
 export function isObject (obj) {
-    return obj !== null && typeof obj === 'object'
+  return obj !== null && typeof obj === 'object'
 }
 /**
  * Do Nothing.
@@ -18,52 +18,52 @@ export function isObject (obj) {
 let noop = (a) => {}
 export let err = noop
 export let recommend = noop
-if('ENV' !== "production"){
-    /**
+if ('ENV' !== 'production') {
+  /**
      * Notified with an error message
      */
-    err = (msg) => {
-        hasConsole && inBrowser
-        ? console.info(`%c[Reactif error]: %c` + msg,'font-weight:bold;color:red;','font-weight:bold;color:black')
-        /* istanbul ignore next */
-        : console.info(`[Reactif error]:` + msg)
-        return false
-    }
-    /**
+  err = (msg) => {
+    hasConsole && inBrowser
+      ? console.info(`%c[Reactif error]: %c` + msg, 'font-weight:bold;color:red;', 'font-weight:bold;color:black')
+    /* istanbul ignore next */
+      : console.info(`[Reactif error]:` + msg)
+    return false
+  }
+  /**
      * Notified with a recommendation message
      */
-    recommend = (msg) => {
-        hasConsole && inBrowser
-        ? console.info(`%c[Reactif ${$$Variables.__version__}]: %c` + msg,'font-weight:bold;color:green;','font-weight:bold;color:black')
-        /* istanbul ignore next */
-        : console.info(`[Reactif ${$$Variables.__version__}]:` + msg)
-        return true
-    }
+  recommend = (msg) => {
+    hasConsole && inBrowser
+      ? console.info(`%c[Reactif ${$$Variables.__version__}]: %c` + msg, 'font-weight:bold;color:green;', 'font-weight:bold;color:black')
+    /* istanbul ignore next */
+      : console.info(`[Reactif ${$$Variables.__version__}]:` + msg)
+    return true
+  }
 }
 /**
  * Create a Map within standards used
  */
-export function createMap(boundry){
-    boundry.getter = {}
-    boundry.setter = {}
-    boundry.deleter = {}
-    return boundry
+export function createMap (boundry) {
+  boundry.getter = {}
+  boundry.setter = {}
+  boundry.deleter = {}
+  return boundry
 }
 /**
  * Create an Array Map within standards used
  */
-export function createArrayMap(boundry){
-    boundry.getter = []
-    boundry.setter = []
-    boundry.deleter = []
-    return boundry
+export function createArrayMap (boundry) {
+  boundry.getter = []
+  boundry.setter = []
+  boundry.deleter = []
+  return boundry
 }
 /**
  * Add an property into a Map
  */
-export function addPropToMap(map,name,func){
-    if(!map.hasOwnProperty(name)) map[name] = []
-        map[name].push(func)
+export function addPropToMap (map, name, func) {
+  if (!map.hasOwnProperty(name)) map[name] = []
+  map[name].push(func)
 }
 /**
  * Observables are of Functional Reactive Programming,
@@ -72,60 +72,60 @@ export function addPropToMap(map,name,func){
  * @export
  * @class Observable
  */
-export class Observable{
-    constructor(subscriberFunction){
-        this.subscriberFunction = subscriberFunction;
-    }
-    subscribe(observer){
-        this.subscriberFunction(observer)
-    }
+export class Observable {
+  constructor (subscriberFunction) {
+    this.subscriberFunction = subscriberFunction
+  }
+  subscribe (observer) {
+    this.subscriberFunction(observer)
+  }
 }
 /* istanbul ignore next */
-export function depManager(map,reactable,commonMap,states){
-    let get = (name,func) =>{
-        addPropToMap(map.getter,name,func)
+export function depManager (map, reactable, commonMap, states) {
+  let get = (name, func) => {
+    addPropToMap(map.getter, name, func)
+  }
+  let set = (name, func) => {
+    addPropToMap(map.setter, name, func)
+  }
+  let del = (name, func) => {
+    addPropToMap(map.deleter, name, func)
+  }
+  let add = (name, value) => {
+    reactable[name] = value
+    states[name] = value
+  }
+  let common = (type, func) => {
+    switch (type) {
+      case 'get':
+        commonMap['getter'].push(func)
+        break
+      case 'set':
+        commonMap['setter'].push(func)
+        break
+      case 'del':
+        commonMap['deleter'].push(func)
+        break
+        /* istanbul ignore next */
+      default:
+        err('Illegally using common Function')
     }
-    let set = (name,func) =>{
-        addPropToMap(map.setter,name,func)
-    }
-    let del =(name,func) =>{
-        addPropToMap(map.deleter,name,func)
-    }
-    let add = (name,value) =>{
-        reactable[name] = value
-        states[name] = value
-    }
-    let common = (type,func) =>{
-        switch(type){
-            case 'get':
-            commonMap['getter'].push(func)
-            break;
-            case 'set':
-            commonMap['setter'].push(func)
-            break;
-            case 'del':
-            commonMap['deleter'].push(func)
-            break;
-            /* istanbul ignore next */
-            default:
-            err('Illegally using common Function')
-        }
-    }
-    let past = name =>{
-        return name ? states[name] : false;
-    }
-    return {get,set,del,add,common,past}
+  }
+  let past = name => {
+    return name ? states[name] : false
+  }
+  return { get, set, del, add, common, past }
 }
 
 /* istanbul ignore next */
-export function createObservableMap(map){
-    let res = createMap({})
-    for(let prop in res){
-            for(let key in map[prop]){
-                res[prop][key] = new Observable(function(observer){
-                   map[prop][key].forEach(x =>{ observer.next(x) })
-                })
-            }
+export function createObservableMap (map) {
+  let res = createMap({})
+  for (let prop in res) {
+    for (let key in map[prop]) {
+      res[prop][key] = new Observable(function (observer) {
+        map[prop][key].forEach(x => { observer.next(x) })
+      })
     }
-    return res
+  }
+  return res
 }
